bec811ca0c9b06a4531901f07201fb14f4b693a2
Vladimir Panchenko <dropsonic.pan@gmail.com>
2012-07-12 11:11:04 +0000

    Поправил расположение файлов FarseerTools и ссылки.

--- GameLogic/GameObject.cs
+++ GameLogic/GameObject.cs
@@ -6,6 +6,7 @@ using FarseerPhysics.Dynamics;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using FarseerTools;
 
 namespace GameLogic
 {
