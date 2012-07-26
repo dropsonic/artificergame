4ad0d2d328e7fb44c81e0691e261521340c056d0
Vladimir Panchenko <dropsonic.pan@gmail.com>
2012-07-26 09:51:41 +0000

    Корректная работа механизма undo/redo - теперь после многочисленных undo и выполнении новой команды список redo очищается.

--- LevelEditor/Commands/CommandManager.cs
+++ LevelEditor/Commands/CommandManager.cs
@@ -11,12 +11,21 @@ namespace LevelEditor.Commands
         private List<ICommand> _executedCommandList = new List<ICommand>();
         private int _currentCommandIndex = -1;
 
+        /// <summary>
+        /// Добавляет команду в список команд.
+        /// </summary>
+        /// <param name="command"></param>
         public void AddCommand(ICommand command)
         {
             _commands.Add(command.Name,command); 
         }
 
-        public void Execute(ICommand command)
+        /// <summary>
+        /// Выполняет указанную команду.
+        /// </summary>
+        /// <param name="command">Команда для выполнения.</param>
+        /// <param name="redo">true, если команда вызывается как Redo; false в ином случае.</param>
+        private void Execute(ICommand command, bool redo)
         {
             try
             {
@@ -24,11 +33,22 @@ namespace LevelEditor.Commands
                 //Если это команда, поддерживающая механизм undo/redo, то добавляем её в список выполненных команд
                 if (command is IUndoRedoCommand)
                 {
-                    //Если мы в конце списка (Redo не делалось)
-                    if (_currentCommandIndex >= _executedCommandList.Count - 1)
-                        _executedCommandList.Add(command);
-                    else
+                    if (redo)
+                    {
                         _executedCommandList[_currentCommandIndex + 1] = command;
+                    }
+                    else
+                    {
+                        if (_currentCommandIndex >= _executedCommandList.Count - 1)
+                            _executedCommandList.Add(command);
+                        else
+                        {
+                            _executedCommandList[_currentCommandIndex + 1] = command;
+                            int deleteIndex = _currentCommandIndex + 2;
+                            int deleteCount = _executedCommandList.Count - deleteIndex;
+                            _executedCommandList.RemoveRange(deleteIndex, deleteCount);
+                        }
+                    }
 
                     _currentCommandIndex++;
                 }
@@ -39,6 +59,19 @@ namespace LevelEditor.Commands
             }
         }
 
+        /// <summary>
+        /// Выполняет указанную команду.
+        /// </summary>
+        /// <param name="command">Команда для выполнения.</param>
+        public void Execute(ICommand command)
+        {
+            Execute(command, false);
+        }
+
+        /// <summary>
+        /// Выполняет команду с указанным именем из списка команд CommandManager'a.
+        /// </summary>
+        /// <param name="commandName">Имя команды.</param>
         public void Execute(string commandName)
         {
             try
@@ -52,6 +85,9 @@ namespace LevelEditor.Commands
             }
         }
 
+        /// <summary>
+        /// Отменяет предыдущую команду.
+        /// </summary>
         public void Undo()
         {
             if (CanUndo)
@@ -69,10 +105,13 @@ namespace LevelEditor.Commands
             }
         }
 
+        /// <summary>
+        /// Повторяет последнюю отменённую команду.
+        /// </summary>
         public void Redo()
         {
             if (CanRedo)
-                Execute(_executedCommandList[_currentCommandIndex+1]);
+                Execute(_executedCommandList[_currentCommandIndex+1], true);
         }
 
         public bool CanUndo
