086f3ce7de4e028345063449f5657474190d2f73
Vladimir Panchenko <dropsonic.pan@gmail.com>
2012-07-30 18:31:11 +0000

    Сделал сериализацию метаданных текстуры.

--- Game/FarseerTools/Serializing/ObjectMetadataDictionary.cs
+++ Game/FarseerTools/Serializing/ObjectMetadataDictionary.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.ComponentModel;
 
-namespace LevelEditor
+namespace FarseerTools
 {
     public class ObjectMetadataDictionaryAttribute : Attribute
     {
---
+++ Game/FarseerTools/Serializing/TextureMetadataKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FarseerTools
+{
+    /// <summary>
+    /// Содержит ключи для словаря метаданных текстур.
+    /// </summary>
+    public static class TextureMetadataKeys
+    {
+        public const string TextureFileName = "TextureFileName";
+    }
+}
